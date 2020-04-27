164e538158bef7a660e319e9a04b723e95c23866
@higakikeita <higaki.keichan@gmail.com>
2020-04-27 22:11:32 +0900

    アソシエーションの設定

--- db/migrate/20200427130155_create_reports.rb
+++ db/migrate/20200427130155_create_reports.rb
@@ -1,7 +1,10 @@
 class CreateReports < ActiveRecord::Migration[5.0]
   def change
     create_table :reports do |t|
-
+      t.string :name
+      t.integer :bags
+      t.datetime :registered_at, null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200426145859) do
+ActiveRecord::Schema.define(version: 20200427130155) do
+
+  create_table "reports", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "bags"
+    t.datetime "registered_at", null: false
+    t.integer  "user_id",       null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["user_id"], name: "index_reports_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "caddy_number",           default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 20200426145859) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "reports", "users"
 end
